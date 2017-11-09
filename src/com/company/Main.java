5502f95144d0636e9e295d8b948ad4250a61665e
Susy <cmsusana98@gmail.com>
2017-11-09 16:24:33 -0600

    Size

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,8 @@
 package com.company;
 
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -8,7 +10,54 @@ public class Main {
         boolean stop = false;
         Pilas pila = new Pilas();
 
+        Scanner sc=new Scanner(System.in);
 
+        boolean seguir=true;
+        while(seguir) {
+            pila.Menu();
+            String op = sc.next();
+            switch (op) {
+                case "1":
+                case "Insertar":
+                case "insertar":
+                    System.out.println("Usted eligio la opci�n Insertar" +
+                            "�Cuantos valores desea insertar?");
+                    int num = sc.nextInt();
+                    for (int i=0; i<num; i++){
+                        System.out.println("Valor "+(i+1)+": ");
+                        int valor=sc.nextInt();
+                        pila.push(valor);
+                    }
+                    break;
+                case "2":
+                case "Mostrar":
+                case "mostrar":
+                    System.out.println("Usted eligio la opci�n Mostrar\n");
+                        pila.peek();
+                    
+                    break;
+                case "3":
+                case "Buscar":
+                case "buscar":
+                    
+                    break;
+                case "4":
+                case "Eliminar":
+                case "eliminar":
+                   pila.pop();
+                    break;
+                case "5":
+                case "Contar":
+                case "contar":
+                    System.out.println("La lista contiene "+pila.Size()+" elementos");
+                    break;
+                case "6":
+                case "Terminar":
+                case "terminar":
+                    System.out.println("Usted eligio la opci�n Terminar\n\nAdios ...! vuelva pronto XD");
+                      seguir=false;
+            }
+        }
 
 
     }
